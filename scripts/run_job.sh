4932b814cb06467d4cf93e15e3808ecb14d42fbc
Iliya Vereshchagin <i.vereshchagin@gmail.com>
2017-08-30 06:42:43 -0700

    scripts modified

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -5,5 +5,5 @@ if [ ! $? = 0 ]; then
     echo "Job started on "`date +%Y%m%d-%H%M%S` >>$HOME/devops_ce/logs/scheduler.log 2>&1
     $HOME/devops_ce/scripts/first_try.sh
 else
-    echo "Nothing to update, job stopped" >>$HOME/devops_ce/logs/scheduler.log 2>&1
+    echo "Nothing to update, job stopped" >/dev/null 2>&1
 fi
